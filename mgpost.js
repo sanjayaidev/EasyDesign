c3a845f0cdb1ddb32cedc7ffd8d518896ff5cda6
sanjayaidev <testhimrealty@gmail.com>
2026-07-22 15:18:34 +0530

    Update mgpost.js

--- mgpost.js
+++ mgpost.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const { Pool } = require('pg');
+import fs from 'fs';
+import pg from 'pg';
+const { Pool } = pg;
 
 // Railway automatically injects DATABASE_URL
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
